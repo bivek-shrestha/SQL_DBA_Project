c3a168f3b78d10e581bd8b5e4656dd4dc206d1f9
Bivek Shrestha <bivekshrestha00@gmail.com>
2024-03-01 14:01:06 +0545

    Update dba_posuserprivelleges.sql

--- dba_posuserprivelleges.sql
+++ dba_posuserprivelleges.sql
@@ -1,4 +1,3 @@
 
-GRANT ALL PRIVILEGES ON *.* TO 'admin_bivek'@'localhost';
-GRANT SELECT, INSERT, UPDATE, DELETE ON DBA_Project_bivek.* TO 'cashier1'@'localhost';
-GRANT SELECT ON DBA_Project_bivek.* TO 'sales1'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'bivek_dba'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE ON DBA_Project_bivek.* TO 'newuser_dba_reviewer'@'localhost';
